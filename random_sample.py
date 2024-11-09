3bc0cb619bd936a24a6b27749d378760973f10d6
CosmosSolitarus <156846497+CosmosSolitarus@users.noreply.github.com>
2024-11-09 00:30:38 -0500

    Updated sample files to remove severity and description columns

--- random_sample.py
+++ random_sample.py
@@ -5,10 +5,10 @@ csv_file = 'us_accidents.csv'
 df = pd.read_csv(csv_file)
 
 # Remove the specified columns
-df = df.drop(columns=['Country', 'Turning_Loop'], errors='ignore')
+df = df.drop(columns=['Severity', 'Country', 'Description', 'Turning_Loop'], errors='ignore')
 
 # Randomly select n rows
 sample_data = df.sample(n=250000, random_state=1)
 
 # Save the sample to a new CSV file
-sample_data.to_csv('us_accidents_sample.csv', index=False)
+sample_data.to_csv('us_accidents_sample_MINI.csv', index=False)
